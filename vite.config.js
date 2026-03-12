b886bdecd61ea2dc4a1b2e85ee1da513b6b5835c
Dani <danilago1998@gmail.com>
2026-03-12 17:29:18 +0000

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+	base: '/A30.-Documentar-y-desplegar-con-CI-una-App-HTML5/',
 	resolve: {
 		alias: {
 			'@': '/src',
 		},
+	},
+	build: {
+		sourcemap: false,
+		minify: 'esbuild',
+		chunkSizeWarningLimit: 2000,
 	},
 })
